52931bb02b670570526315d5eacbc9b168b61ff8
Bartosz Nowak <sztosz@gmail.com>
2015-03-25 23:52:00 +0100

    Basic Models

---
+++ mum/Models/TypeOfExamination.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace mum.Models
+{
+	public class TypeOfExamination
+	{
+		public int TypeOfExaminationId { get; set; }
+		public string Name { get; set; }
+
+		public IList Examinations { get; set; }
+	}
+}
