0d522e451dbac47f9180758f5142ef1515de3378
Bartosz Nowak <sztosz@gmail.com>
2015-03-26 17:41:58 +0100

    Added context

---
+++ mum/Models/Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mum.Models
+{
+	internal class Context : DbContext
+	{
+		public Context() : base()
+		{
+		}
+
+		public DbSet<Examination> Examinations { get; set; }
+		public DbSet<ExaminationsList> ExaminationsLists { get; set; }
+		public DbSet<TypeOfExamination> TypeOfExaminations { get; set; }
+	}
+}
