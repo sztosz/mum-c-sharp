ab16c8745ef23aa2dff5ae417ebea15a008718a5
Bartosz Nowak <sztosz@gmail.com>
2015-03-26 17:42:13 +0100

    Added basic windows

---
+++ mum/Views/ExaminationsListsWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace mum.Views
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void OnAddNewList(object sender, RoutedEventArgs e)
+		{
+			var addListWindow = new AddListWindow();
+			addListWindow.Show();
+		}
+	}
+}
--- mum/Views/ExaminationsWindow.xaml.cs
+++ mum/Views/ExaminationsWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace mum
+namespace mum.Views
 {
 	/// <summary>
-	/// Interaction logic for MainWindow.xaml
+	/// Interaction logic for ExaminationListWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class ExaminationListWindow : Window
 	{
-		public MainWindow()
+		public ExaminationListWindow()
 		{
 			InitializeComponent();
 		}
